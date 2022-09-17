c77d7f2a96973a54fe8a14919031fbed40240924
embarq <embarq.dev@gmail.com>
2022-09-17 09:34:38 +0900

    refactor code structure

--- src/controllers/post-validate-recaptcha.ts
+++ src/controllers/post-validate-recaptcha.ts
@@ -1,25 +1,18 @@
 import { Context } from 'koa'
 import got from 'got'
 import * as accessTokenStore from '../lib/access-token-store'
-
-interface SiteVerifyResponse {
-  "success": boolean,
-  "challenge_ts": number,
-  "hostname": string,
-  "error-codes": unknown[]
-}
+import { ReCaptchaSiteVerifyResponse } from '../model/vendor'
 
 export const handleValidateRecaptcha = async (ctx: Context) => {
   const res = await got
     .post('https://www.google.com/recaptcha/api/siteverify', {
-
       json: {
         secret: process.env.RECAPTCHA_SECRET_KEY,
         response: ctx.request.body.response,
         remoteip: ctx.request.ip
       }
     })
-    .json<SiteVerifyResponse>()
+    .json<ReCaptchaSiteVerifyResponse>()
 
   if (!res.success) {
     console.error(res)
@@ -27,14 +20,12 @@ export const handleValidateRecaptcha = async (ctx: Context) => {
     ctx.body = {
       status: 'error',
       payload: {
-        detail: res['error-codes']
+        code: res['error-codes'],
       }
     }
     return
   }
 
-  // TODO: remove
-  console.log(res)
   const token = await accessTokenStore.create({ clientIP: ctx.request.ip })
 
   ctx.body = {
