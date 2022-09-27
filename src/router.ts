240b77aa370d67d188701e2565c11f4774bc49fe
embarq <embarq.dev@gmail.com>
2022-09-27 18:05:04 +0900

    fix auth middleware

--- src/router.ts
+++ src/router.ts
@@ -10,5 +10,6 @@ export const router = new Router<unknown, AppContext>()
 
 router.get('/', handleIndex)
 router.post('/v1/validate-recaptcha', handleValidateRecaptcha)
-router.post('/v1/image/upload', tempAuthMiddleware, handleImageUpload)
+router.use('/v1/image/upload', tempAuthMiddleware)
+router.post('/v1/image/upload', handleImageUpload)
 router.get('/v1/image/:name', handleGetImage)
